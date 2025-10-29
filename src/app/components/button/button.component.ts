9c0059546bed6f3b303922c78f07a45977b81bf0
abdulrahmankhalled <abdullraahmaankhalled@gmail.com>
2025-10-29 19:35:22 +0300

    Revert "DS fixes"
    This reverts commit 3efc81cd91947cab8ea5f3e7cf54113bc1a15a62.

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -12,15 +12,11 @@ import { ButtonModule } from 'primeng/button';
       [icon]="icon"
       [iconPos]="iconPos"
       [severity]="severity"
-      [outlined]="outlined"
-      [text]="text"
-      [raised]="raised"
-      [rounded]="rounded"
-      [size]="size"
       [disabled]="disabled"
       [loading]="loading"
       [loadingIcon]="loadingIcon"
       [badge]="badge"
+      [badgeClass]="badgeClass"
       (onClick)="onClick.emit($event)"
       (onFocus)="onFocus.emit($event)"
       (onBlur)="onBlur.emit($event)"
@@ -38,17 +34,8 @@ export class ButtonComponent {
   @Input() label: string = '';
   @Input() icon: string = '';
   @Input() iconPos: 'left' | 'right' | 'top' | 'bottom' = 'left';
-  @Input() severity: 'primary' | 'secondary' | 'success' | 'info' | 'warn' | 'help' | 'danger' = 'primary';
+  @Input() severity: 'primary' | 'secondary' = 'primary';
   @Input() disabled: boolean = false;
-  @Input() outlined: boolean = false;
-  @Input() text: boolean = false;
-  @Input() styleClass: string = '';
-
-  @Input() raised: boolean = false;
-  @Input() link: boolean = false;
-  @Input() rounded: boolean = false;
-  @Input() fluid: boolean = false;
-  @Input() size: 'small' | 'large' = 'small';
   @Input() loading: boolean = false;
   @Input() loadingIcon: string = 'pi pi-spinner pi-spin';
   @Input() badge: string = '';
@@ -57,5 +44,4 @@ export class ButtonComponent {
   @Output() onClick = new EventEmitter<any>();
   @Output() onFocus = new EventEmitter<any>();
   @Output() onBlur = new EventEmitter<any>();
-
 }
